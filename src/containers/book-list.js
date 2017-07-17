ef63693570608516644ac6599ae5712a98583e41
Steven Starnes <starnes81@gmail.com>
2017-07-16 19:25:35 -0500

    Resolve local issues

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -4,7 +4,7 @@ export default class BookList extends Component {
 	renderList() {
 		return this.props.books.map((book) => {
 			return (
-				<li key{book.title} className="list-group-item">{book.title}</li>
+				<li key={book.title} className="list-group-item">{book.title}</li>
 			);
 		});
 	}
