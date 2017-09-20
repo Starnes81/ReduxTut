a81e89a6d4a5566dd27eb9b5d98d6c9134187233
Steven Starnes <starnes81@gmail.com>
2017-09-20 14:38:06 -0500

    binded action
    Set up imports and added map functions to bind action

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
+import {selectBook} from '../actions/index';
+import {bindActionCreators} from 'redux';
 
 class BookList extends Component {
 	renderList() {
@@ -24,7 +26,10 @@ render() {
 function mapStateToProps(state) {
 	return {
 		books: state.books
-	}
+	};
 }
 
-export default connect(mapStateToProps)(BookList);
+function mapDispatchToProps(dispatch) {
+	return bindActionCreators({ selectBook: selectBook}, dispatch)
+}
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
